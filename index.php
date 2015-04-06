973408f7771dbe2454b34e9ddb8c79b2b047f3f8
LayneB <Layne.Bourque@gmail.com>
2015-04-06 11:44:57 -0700

    Update index.php
    Including PHP directives and proper pretty page formatting

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<html><body><h1>I is epic.</h1>
-<img style="-webkit-user-select: none" src="http://k09.kn3.net/taringa/1/3/2/2/0/4/27/the_mima/A20.gif?1138">
-</body></html>
-
+<html>
+   <body>
+        <h1>
+                Who is epic?
+        </h1>
+        <h2>
+                <?php                                           // Use Use PHP to get and print the date and time.
+                        echo "The date and time is ";           // 'echo' in scripting sends output to screen
+                        echo date('Y-m-d H:i:s');               // so the date command is exexuted, and "echoed" to the screen.
+                ?>
+        </h2>
+        <img style="-webkit-user-select: none" src="http://k09.kn3.net/taringa/1/3/2/2/0/4/27/the_mima/A20.gif?1138">
+        <h3>
+                <?php
+                        echo "Man, Baxter REALLY loves this GIF";
+                ?>
+        </h3>
+   </body>
+</html>
